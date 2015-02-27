d447983c388d53586611b1d5cb8008201442be78
Ben Miller <benito.m@gmail.com>
2015-02-27 13:56:21 +0000

    First submission of the plugin

---
+++ README.md
@@ -0,0 +1,3 @@
+# Discourse Allow Same Origin Plugin
+
+This plugin sets Discourse's Rails server to add the `X-Frame-Options: ALLOWALL` HTTP header to all responses.
---
+++ plugin.rb
@@ -0,0 +1,7 @@
+file: plugins/yoursite/plugin.rb
+# name: allowsameorigin
+# about: allows Discourse site to be embedded in HTML iframe
+# version: 1
+# authors: Ben Miller
+
+Rails.application.config.action_dispatch.default_headers.merge!({'X-Frame-Options' => 'ALLOWALL'})
